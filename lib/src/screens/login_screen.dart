656d1519ac76baaedfe174602674b4d514c06d35
Evgeniy <edubskiy@openwaygroup.com>
2020-04-16 00:54:54 +0300

    Added Form fields

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -7,9 +7,39 @@ class LoginScreen extends StatelessWidget {
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-          
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton()
         ],
       ),
     );
   }
+}
+
+Widget emailField() {
+  return TextField(
+    keyboardType: TextInputType.emailAddress,
+    decoration: InputDecoration(
+      hintText: 'you@example.com',
+      labelText: 'Email address'
+    ),
+  );
+}
+
+Widget passwordField() {
+  return TextField(
+    decoration: InputDecoration(
+      hintText: 'Password',
+      labelText: 'Password'
+    ),
+  );
+}
+
+Widget submitButton() {
+  return RaisedButton(
+    child: Text('Login'),
+    color: Colors.blue,
+    onPressed: () {},
+  );
 }
